7c757f935298c420eb711516bd46659353bc4832
houxin <183188574@qq.com>
2019-04-05 19:13:12 +0800

    诗词大会

--- src/test/java/com/school/union/poetry/PoetryApplicationTests.java
+++ src/test/java/com/school/union/poetry/PoetryApplicationTests.java
@@ -1,8 +1,5 @@
 package com.school.union.poetry;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.school.union.poetry.entity.AnswerRecord;
-import com.school.union.poetry.mapper.AnswerRecordMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,12 +9,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class PoetryApplicationTests {
 
-    private AnswerRecordMapper answerRecordMapper;
-
     @Test
     public void contextLoads() {
 
-        answerRecordMapper.selectList(new QueryWrapper<>(new AnswerRecord()));
     }
 
 }
