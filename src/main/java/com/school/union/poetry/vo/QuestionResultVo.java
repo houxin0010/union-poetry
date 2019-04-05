80baad56c92bb600632e9f14ddd763f041fdb56e
houxin <183188574@qq.com>
2019-04-05 20:46:45 +0800

    诗词大会

--- src/main/java/com/school/union/poetry/vo/QuestionResultVo.java
+++ src/main/java/com/school/union/poetry/vo/QuestionResultVo.java
@@ -3,6 +3,7 @@ package com.school.union.poetry.vo;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * QuestionResultVo
@@ -27,7 +28,15 @@ public class QuestionResultVo implements Serializable {
      */
     private String answer;
     /**
+     * 选项
+     */
+    private List<String> options;
+    /**
      * 题目类型，选择题：SINGLE_SEL，填空题：COMPLETION，选择题：TRUE_OR_FALSE，选字填空：BANKED_CLOZE
      */
     private String questionType;
+    /**
+     * 题目数量
+     */
+    private Integer questionTotal;
 }
