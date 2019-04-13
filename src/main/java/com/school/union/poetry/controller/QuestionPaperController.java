70988edb956181048aada5fce7c5a65acaf51751
houxin <183188574@qq.com>
2019-04-13 14:57:31 +0800

    诗词大会

--- src/main/java/com/school/union/poetry/controller/QuestionPaperController.java
+++ src/main/java/com/school/union/poetry/controller/QuestionPaperController.java
@@ -1,6 +1,7 @@
 package com.school.union.poetry.controller;
 
 import com.school.union.poetry.vo.AnswerResultVo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import com.school.union.poetry.vo.QuestionPaperInitVo;
 import com.school.union.poetry.vo.QuestionResultVo;
 import com.school.union.poetry.vo.base.ResultVo;
 
+@Slf4j
 @RestController
 @RequestMapping("/questionPaper")
 public class QuestionPaperController {
@@ -24,16 +26,15 @@ public class QuestionPaperController {
         String openId = "houxin";
         return ResultVo.success(questionPaperService.createQuestionPaper(openId));
     }
-    
+
     @RequestMapping("/getcode")
     public ResultVo<String> getcode(String code) {
-    	String url="https://api.weixin.qq.com/sns/jscode2session?appid=wx9d98970b60072e66&secret=57ae72018a581f2a15be8ce971036dc0&js_code="+code+"&grant_type=authorization_code";
-        
-       String json = HttpKit.get(url);
-      
-		JSONObject a = JSONObject.parseObject(json);
-		String openid = a.getString("openid");
-		 
+        String url = "https://api.weixin.qq.com/sns/jscode2session?appid=wx9d98970b60072e66&secret=57ae72018a581f2a15be8ce971036dc0&js_code=" + code + "&grant_type=authorization_code";
+
+        String json = HttpKit.get(url);
+        log.info("getcode json = {}", json);
+        JSONObject a = JSONObject.parseObject(json);
+        String openid = a.getString("openid");
         return ResultVo.success(openid);
     }
 
